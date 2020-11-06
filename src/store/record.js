59f47d0ef010899dffb9f863c33d03fc9eb77279
denzelwash <denzelwash33@mail.ru>
2020-11-06 14:12:47 +0300

    finished half of planning

--- src/store/record.js
+++ src/store/record.js
@@ -1,11 +1,24 @@
 import firebase from 'firebase/app'
 import errors from '@/utils/firebase-error-messages'
+
 export default {
   actions: {
     async addRecord({dispatch, commit}, payload) {
       try {
         const uid = await dispatch('getUid')
         const newRecord = await firebase.database().ref(`users/${uid}/records`).push(payload)
+      } catch(e) {
+        commit('setError', err)
+        throw err
+      }
+    },
+    async getRecords({dispatch, commit}) {
+      try {
+        const uid = await dispatch('getUid')
+        const records = (await firebase.database().ref(`users/${uid}/records`).once('value')).val()
+        return records ?
+          Object.keys(records).map((key) => ({...records[key], id: key})) :
+          []
       } catch(e) {
         commit('setError', err)
         throw err
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,26 +2,72 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ info.bill || '' | currencyFilter }}</h4>
     </div>
 
     <section>
-      <div>
-        <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
-        </p>
-        <div class="progress">
-          <div class="determinate green" style="width:40%"></div>
+      <Loader v-if="loading"/>
+      <div v-else-if="categories.length">
+        <div
+          v-for="item in categories"
+          :key="item.title"
+        >
+          <p>
+            <strong>{{ item.title }}</strong>
+            {{ item.balance }} из {{ item.limit }}
+          </p>
+          <div class="progress">
+            <div class="determinate green" :style="`width:${item.percents}%`"></div>
+          </div>
         </div>
+      </div>
+      <div v-else class="page-subtitle">
+        <h4>Категорий нет</h4>
+        <router-link :to="{name: 'Categories'}" tag="button" class="waves-effect waves-light btn">
+          <span>Добавить</span>
+        </router-link>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-export default {
+import Loader from '@/components/loader.vue'
+import {mapGetters} from 'vuex'
 
+export default {
+  components: {
+    Loader
+  },
+  data: () => ({
+    loading: true,
+    records: [],
+    categories: []
+  }),
+  computed: {
+    ...mapGetters(['info'])
+  },
+  async mounted() {
+    try {
+      const records = await this.$store.dispatch('getRecords')
+      let categories = await this.$store.dispatch('getCategories')
+
+      categories = categories.map(elem => {
+        const filteredRecords = records.filter(rec => (rec.category === elem.id && rec.type === 'outcome'))
+        const catSumm = filteredRecords.reduce((total, current) => total - current.summ, elem.limit)
+        let percents = 100 - catSumm / (elem.limit / 100)
+        percents = percents >= 100 ? 100 : percents
+        return {
+          ...elem,
+          percents,
+          balance: catSumm,
+        }
+      })
+      
+      this.categories = categories
+      this.loading = false
+    } catch(e) {}
+  }
 }
 </script>
 
